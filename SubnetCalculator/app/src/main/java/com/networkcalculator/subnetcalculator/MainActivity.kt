b3fa9c71a3864bd8ae57dbf36b3167053c9b1b30
Victor <victoryang2005@outlook.com>
2023-07-02 16:35:15 -0700

    most of the results data works

--- SubnetCalculator/app/src/main/java/com/networkcalculator/subnetcalculator/MainActivity.kt
+++ SubnetCalculator/app/src/main/java/com/networkcalculator/subnetcalculator/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
@@ -21,6 +25,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
@@ -133,20 +138,35 @@ fun NetworkCalculatorLayout(modifier: Modifier = Modifier) {
         SubnetMaskDropdownMenu()
         HostsDropdownMenu()
         Spacer(modifier = Modifier.height(32.dp))
-        Text(
-            text = "Network: $ipInput/$maskBits" +
-                    "\nNetmask: $subnetMask" +
-                    "\nIp range: $ipInput" +
-                    "\n$ipInput" +
-                    "\nHosts/Net: $hosts" +
-                    "\nBroadcast: $ipInput",
-            modifier = Modifier
-                .padding(bottom = 16.dp)
-        )
+        ResultsDataTable(modifier = Modifier.padding(bottom = 32.dp))
         Spacer(modifier = Modifier.height(150.dp))
     }
 }
 
+@Composable
+fun ResultsDataTable(modifier: Modifier = Modifier) {
+    val broadcast = ipInput.split(".").toTypedArray()
+    val updatedResultsData: List<Pair<String, String>> = listOf(
+        "Network: " to "$ipInput/$maskBits",
+        "Netmask: " to subnetMask,
+        "Ip range: " to ipInput,
+        "Hosts/Net: " to "$hosts",
+        "Broadcast: " to ipInput
+    )
+
+    LazyColumn {
+        itemsIndexed(updatedResultsData) { index, item ->
+            Row(modifier = Modifier.padding(8.dp)) {
+                Text(text = item.first, modifier = Modifier.weight(1f))
+                Text(text = item.second, modifier = Modifier.weight(1f))
+            }
+
+            if (index < updatedResultsData.size - 1) {
+                Divider(modifier = Modifier.padding(horizontal = 8.dp))
+            }
+        }
+    }
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -158,7 +178,6 @@ fun MaskBitsDropdownMenu(modifier: Modifier = Modifier) {
     maskBits = a
     subnetMask = b
     hosts = c
-    println("MaskBitsDropdown")
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = {
@@ -206,7 +225,6 @@ fun SubnetMaskDropdownMenu(modifier: Modifier = Modifier) {
     maskBits = a
     subnetMask = b
     hosts = c
-    println("SubnetMaskDropdown")
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = {
@@ -254,7 +272,6 @@ fun HostsDropdownMenu(modifier: Modifier = Modifier) {
     maskBits = a
     subnetMask = b
     hosts = c
-    println("HostsDropdown")
     ExposedDropdownMenuBox(
         expanded = expanded,
         onExpandedChange = {
